dd6a4b58952f0a5adaf0f10269fc5402ed893c25
Kaushal <Kaushalkanar29@gmail.com>
2024-04-05 17:33:30 +0530

    Branch checkout

--- add.cpp
+++ add.cpp
@@ -5,6 +5,11 @@ int add(int a, int b) {
     return a + b;
 }
 
+// Function to multiply two numbers
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
     int num1 = 5;
     int num2 = 7;
@@ -12,8 +17,12 @@ int main() {
     // Calling the add function
     int sum = add(num1, num2);
     
-    // Displaying the result
+    // Calling the multiply function
+    int product = multiply(num1, num2);
+    
+    // Displaying the results
     std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+    std::cout << "The product of " << num1 << " and " << num2 << " is: " << product << std::endl;
     
     return 0;
 }
